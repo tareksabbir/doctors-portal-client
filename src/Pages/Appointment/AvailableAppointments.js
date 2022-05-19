03ccf3e216c819d25b3b25d7429187d7b7dfd317
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-05-19 11:03:21 +0600

    handel slots and react query

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,16 +1,28 @@
 import { format } from 'date-fns';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import BookingModal from './BookingModal';
 import Service from './Service';
+import { useQuery } from 'react-query'
+import Loading from '../Shared/Loading';
 
 const AvailableAppointments = ({ date }) => {
-    const [services, setServices] = useState([])
+    // const [services, setServices] = useState([])
     const [treatment, setTreatment] = useState(null)
-    useEffect(() => {
-        fetch('http://localhost:5000/service')
+    const formattedDate = format(date, 'PP')
+    // useEffect(() => {
+    //     fetch(`http://localhost:5000/available?date=${formattedDate}`)
+    //         .then(res => res.json())
+    //         .then(data => setServices(data));
+    // }, [formattedDate])
+
+    const { data: services, isLoading, refetch } = useQuery(['available', formattedDate], () =>
+        fetch(`http://localhost:5000/available?date=${formattedDate}`)
             .then(res => res.json())
-            .then(data => setServices(data));
-    })
+    )
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <>
             <section className='px-20'>
@@ -21,7 +33,7 @@ const AvailableAppointments = ({ date }) => {
                 </div>
                 <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-auto'>
                     {
-                        services.map(service => <Service
+                        services?.map(service => <Service
                             key={service._id}
                             service={service}
                             setTreatment={setTreatment}
@@ -32,7 +44,8 @@ const AvailableAppointments = ({ date }) => {
                 {treatment && <BookingModal
                     date={date}
                     treatment={treatment}
-                    setTreatment={setTreatment}></BookingModal>}
+                    setTreatment={setTreatment}
+                    refetch={refetch}></BookingModal>}
             </section>
         </>
     );
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -4,7 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import Swal from 'sweetalert2';
 
-const BookingModal = ({ treatment, date, setTreatment }) => {
+const BookingModal = ({ treatment, date, setTreatment, refetch }) => {
     const { _id, name, slots, doctor } = treatment;
     const [user] = useAuthState(auth);
     const formattedDate = format(date, 'PP')
@@ -32,7 +32,7 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
+
                 if (data.success) {
                     Swal.fire(
                         'Congratulations!!',
@@ -46,6 +46,7 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
                         'error'
                     )
                 }
+                refetch();
                 setTreatment(null);
             })
 
